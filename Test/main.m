f533a6cbeda38709b773ed0a0596cc94d1277d7b
Rimas Misevičius <rmisev3@gmail.com>
2017-03-24 12:40:29 +0200

    Initial test project

---
+++ Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Rimas on 2017-03-24.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
