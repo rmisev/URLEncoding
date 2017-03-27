5a831c3d9982b98ffd8d01e881ef31a70c2385d0
Rimas Misevičius <rmisev3@gmail.com>
2017-03-27 23:32:17 +0300

    Style: trim trailing space

--- URLEncoding/NSString+URLEncoding.m
+++ URLEncoding/NSString+URLEncoding.m
@@ -38,15 +38,15 @@ static NSCharacterSet *allowedInFragment = nil;
         NSRange rangeNotC0;
         rangeNotC0.location = 0x20;
         rangeNotC0.length = 0x7f - 0x20;
-        
+
         // https://url.spec.whatwg.org/#path-percent-encode-set
         allowedInPath = [NSMutableCharacterSet characterSetWithRange:rangeNotC0];
         [allowedInPath removeCharactersInString:@" \"#<>?`{}"];
-        
+
         // https://url.spec.whatwg.org/#query-state
         allowedInQuery = [NSMutableCharacterSet characterSetWithRange:rangeNotC0];
         [allowedInQuery removeCharactersInString:@"\x22\x23\x3C\x3E"];
-        
+
         // https://url.spec.whatwg.org/#c0-control-percent-encode-set
         allowedInFragment = [NSCharacterSet characterSetWithRange:rangeNotC0];
     }
@@ -89,7 +89,7 @@ static NSCharacterSet *allowedInFragment = nil;
     if (r.location == NSNotFound) return nil;
     const NSRange rangeOfScheme = NSMakeRange(0, r.location);
     const NSInteger endOfProtocol = r.location + 1; // skip ":"
-    
+
     // is the scheme supported?
     NSString *scheme = nil;
     for (NSString *specScheme in schemePortDic) {
@@ -99,7 +99,7 @@ static NSCharacterSet *allowedInFragment = nil;
         }
     }
     if (scheme == nil) return self;
-    
+
     // find end of slashes
     NSInteger endOfSlashes = endOfProtocol;
     while (endOfSlashes < self.length) {
@@ -112,7 +112,7 @@ static NSCharacterSet *allowedInFragment = nil;
 
     // authority
     const NSRange rangeOfAuth = [self findPart:endOfAuthChars fromIndex:endOfSlashes];
-    
+
     // after authority
     NSRange rangeOfPath = NSMakeRange(0, 0);
     NSRange rangeOfQuery = NSMakeRange(0, 0);
@@ -140,7 +140,7 @@ static NSCharacterSet *allowedInFragment = nil;
             rangeOfFragment = NSMakeRange(indexOfCh, self.length - indexOfCh);
         }
     }
-    
+
     // make normalized URL
     NSMutableString *normUrl = [NSMutableString stringWithCapacity:self.length];
     [normUrl appendString:scheme];
@@ -154,7 +154,7 @@ static NSCharacterSet *allowedInFragment = nil;
         [normUrl appendString:[[self substringWithRange:rangeOfQuery] percentEncodeUrlQuery]];
     if (rangeOfFragment.length > 0)
         [normUrl appendString:[[self substringWithRange:rangeOfFragment] normalizeUrlFragment]];
-    
+
     return normUrl;
 }
 
